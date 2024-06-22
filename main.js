cf2a537f3648b33ccde2fea4ded14172d3ed8789
MDayZZ <maxdayZZOff@gmail.com>
2024-06-22 17:10:10 +0300

    0.1

---
+++ main.js
@@ -0,0 +1,261 @@
+const cardForm = document.querySelector('#card-form')
+const cardNumberInput = document.querySelector('#card__number-input')
+const cardHolderInput = document.querySelector('#card__holder-input')
+const cardExpDateInput = document.querySelector('#card__exp-date-input')
+const cardCvvInput = document.querySelector('#card__cvv-input')
+const cardLogo = document.querySelector('#card_logo')
+const cardLogoAnimationTime = window.getComputedStyle(cardLogo).animationDuration.replace(/[ms]/g,'')
+const dateNow = new Date(Date.now())
+const yearNow = dateNow.getFullYear()
+const monthNow = dateNow.getMonth()
+
+
+let cardNumberValid = false;
+let cardHolderValid = false;
+let cardExpDateValid = false;
+
+const formDataToJson = (formData) => {
+    const json = {};
+    formData.forEach((value, key) => {
+        json[key] = value.trim();
+    });
+    json['card_number'] = json['card_number'].replace(/ /g, '')
+    return JSON.stringify(json);
+};
+
+const akaFetch = (data) => {
+    const result = {
+        ok: false,
+        status: 404,
+    }
+    return new Promise((resolve, rejected) => {
+        setTimeout(() => {
+            if (data) {
+                result.ok = true;
+                result.status = 200;
+                resolve(result)
+            } else {
+                rejected(result)
+            }
+        }, 3000)
+    })
+}
+
+
+const sendData = () => {
+    const statusElement = document.querySelector('#card__status')
+    statusElement.hidden = true
+    if(
+        cardForm.checkValidity()
+        // cardNumberInput.checkValidity() &&
+        // cardHolderInput.checkValidity() &&
+        // cardExpDateInput.checkValidity() &&
+        // cardCvvInput.checkValidity()
+    ) {
+        createOverlayElement()
+        const formData = new FormData(cardForm)
+        const jsonData = formDataToJson(formData)
+        console.log(jsonData)
+
+        akaFetch()
+            .then(response => {
+                console.log(response)
+                createSuccessfullyOverlayElement()
+                setTimeout(()=>{
+                    location.reload()
+                }, 5000)
+
+
+            })
+            .catch(response => {
+                statusElement.textContent = 'Ошибка сервера'
+                statusElement.hidden = false;
+
+                console.log(response)
+            })
+            .finally(() => {
+                hideOverlay()
+            })
+    }
+    else {
+        cardForm.reportValidity()
+        statusElement.textContent = 'Ошибка: Проверьте поля ввода'
+        statusElement.hidden = false;
+    }
+
+}
+
+cardNumberInput.addEventListener('input', function (event) {
+    let value = event.target.value;
+    const selectionStart = this.selectionStart;
+    const selectionEnd = this.selectionEnd;
+    value = value.replace(/\D/g, '');
+    if (value.length > 0) {
+        if (value[0] === '4'){
+            if(!cardLogo.src.includes('logo-visa.png')) {
+                console.log(cardLogo.src)
+                changeLogo('./images/logo-visa.png')
+            }
+        } else if (value[0] === '5'){
+            if(!cardLogo.src.includes('logo-mastercard.png')) {
+                changeLogo('./images/logo-mastercard.png')
+            }
+        } else if (value.length > 1 && value.slice(0, 2) === '22') {
+            if ( !cardLogo.src.includes('logo-mir.png')) {
+                changeLogo('./images/logo-mir.png')
+            }
+        } else changeLogo('./images/logo-empty.png', true)
+    } else changeLogo('./images/logo-empty.png', true)
+    value = value.replace(/(\d{4})(?=\d)/g, '$1 ')
+
+    if(value.length < 19) {
+        this.classList.remove('card__input--valid')
+        this.classList.add('card__input--not-valid')
+    }
+    else {
+        this.classList.remove('card__input--not-valid')
+        this.classList.add('card__input--valid')
+    }
+
+    event.target.value = value
+    if (selectionStart === selectionEnd) {
+        if (selectionStart % 5 === 0){
+            this.setSelectionRange(selectionStart+1, selectionStart+1)
+        } else {
+            this.setSelectionRange(selectionStart, selectionStart)
+        }
+
+    }
+
+})
+
+cardHolderInput.addEventListener('input', function(event) {
+    let value = event.target.value;
+    value = value.replace(/[^a-zA-Z ]/g, '')
+    value = value.toUpperCase()
+    value = value.replace(/ {2}/, ' ')
+    event.target.value = value
+})
+
+cardExpDateInput.addEventListener('input', function (event) {
+    let value = event.target.value;
+    const selectionStart = this.selectionStart;
+    const selectionEnd = this.selectionEnd;
+    let monthValid = false;
+    let yearValid = false;
+    value = value.replace(/\D/g, '')
+    value = value.replace(/(\d{2})(?=\d)/g, '$1/')
+    if (value.length > 1) {
+        const month = +value.slice(0,2)
+        if (month > 12 || month < monthNow) {
+            this.setCustomValidity('Неправильный месяц')
+            this.reportValidity()
+            monthValid = false;
+        } else {
+            monthValid = true;
+            this.setCustomValidity('')
+        }
+    }
+    if (value.length === 5) {
+        const year = value.slice(3,5)
+        if (year < +String(yearNow).slice(2,4)) {
+            yearValid = false
+            this.setCustomValidity('Неправильный год')
+            this.reportValidity()
+            console.log('badYear')
+        } else {
+            yearValid = true;
+        }
+    }
+
+    if (monthValid && yearValid) {
+        this.classList.remove('card__input--not-valid')
+        this.classList.add('card__input--valid')
+    } else {
+        this.classList.remove('card__input--valid')
+        this.classList.add('card__input--not-valid')
+    }
+    event.target.value = value
+
+    if (selectionStart === selectionEnd) {
+        if (selectionStart === 3) {
+            this.setSelectionRange(selectionStart+1, selectionStart+1)
+
+        }else {
+            this.setSelectionRange(selectionStart, selectionStart)
+
+        }
+
+    }
+})
+
+cardCvvInput.addEventListener('input', function (event) {
+    let value = event.target.value;
+    value = value.replace(/\D/g, '')
+    event.target.value = value;
+
+    if (this.value.length === 3) {
+        this.classList.remove('card__input--not-valid')
+        this.classList.add('card__input--valid')
+        this.setCustomValidity('')
+    } else {
+        this.classList.remove('card__input--valid')
+        this.classList.add('card__input--not-valid')
+        this.setCustomValidity('CVV должен содержать 3 цифры')
+    }
+
+})
+
+function changeLogo(newSrc, dontShow = false) {
+    const divider = dontShow ?1 :2
+    // Добавляем класс hidden для запуска анимации исчезания
+    if (cardLogo.classList.contains('visible')) {
+        cardLogo.classList.remove('visible')
+        cardLogo.classList.add('hidden');
+    }
+
+
+    // Ждем немного времени, чтобы анимация завершилась
+    setTimeout(function () {
+        // Меняем src изображения
+        cardLogo.src = newSrc;
+
+        // Удаляем класс hidden и добавляем класс visible для запуска анимации появления
+        if (!dontShow){
+            cardLogo.classList.remove('hidden');
+            cardLogo.classList.add('visible');
+        }
+    }, cardLogoAnimationTime * 1000 / divider); // 300 миллисекунд - это время, равное продолжительности анимации в CSS
+}
+
+const createOverlayElement = function () {
+    const overlayElement = document.createElement('div')
+    overlayElement.id = 'overlay'
+    overlayElement.classList.add('overlay')
+    overlayElement.innerHTML = '<div class="loader"></div>'
+
+
+    document.body.insertAdjacentElement('afterbegin' ,overlayElement)
+}
+
+function hideOverlay() {
+    const overlay = document.getElementById('overlay');
+    if (overlay) {
+        document.body.removeChild(overlay);
+    }
+}
+
+const createSuccessfullyOverlayElement = function () {
+    const successfullyOverlayElement = document.createElement('div')
+    successfullyOverlayElement.id = 'successfullyOverlay'
+    successfullyOverlayElement.classList.add('successfullyOverlay')
+    successfullyOverlayElement.innerHTML = `
+                <h1 class="success__title">Успешно</h1>
+                <div class="successLogo">
+                    <div class="success__checkmark"></div>
+                    <div class="success__round"></div>
+                </div>`
+
+
+    document.body.insertAdjacentElement('afterbegin' ,successfullyOverlayElement)
+}
---
+++ readme.md
@@ -0,0 +1,70 @@
+# Credit Card Form Project
+
+This project is a practice implementation of a credit card submission form. It allows users to enter their card details and submit the form, simulating a payment process. The project was developed to practice and demonstrate proficiency in front-end development technologies.
+
+## Features
+
+- Responsive credit card form
+- Input formatting and validation for card number, cardholder name, expiration date, and CVV
+- Dynamic card logo change based on card number input
+- Simulated server request for form submission
+- Loading and success animations
+
+## Technologies and Tools Used
+
+- **HTML5**: For structuring the content
+- **CSS3**: For styling the form and adding animations
+- **JavaScript (ES6)**: For handling form validation, animations, and simulated server interaction
+- **Font-Face**: For custom fonts
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/mDayZZ/credit-card-form.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd credit-card-form
+   ```
+3. Open `index.html` in your browser to view the form.
+
+## Usage
+
+- Enter a card number, cardholder name, expiration date, and CVV.
+- The card logo will change dynamically based on the card number (supports Visa, MasterCard, and Mir).
+- Click the "Оплатить" button to submit the form.
+- If the input is valid, a loading animation will be displayed followed by a success animation.
+- If there are any errors, an error message will be shown.
+
+## Project Structure
+
+- `index.html`: Contains the HTML structure of the form.
+- `style.css`: Contains the CSS styles and animations.
+- `main.js`: Contains the JavaScript code for form validation, dynamic logo change, and simulated server interaction.
+- `images/`: Contains the card logos and any other image assets.
+- `fonts/`: Contains the custom fonts used in the project.
+
+## Key Concepts Practiced
+
+- **Form Validation**: Ensuring that the user inputs valid data before submission.
+- **Dynamic Content Update**: Changing the card logo based on the card number input.
+- **Animations**: Adding animations for the loading and success states.
+- **Simulated Server Interaction**: Using `Promise` to simulate server interaction with a fake fetch function.
+
+## Preview
+![Presentation](./screenshots/card-form.gif)
+
+## Screenshots
+
+![Form Initial](./screenshots/card-form-initial.png)
+![Form Input Error](./screenshots/card-form-input-error.png)
+![Loading Animation](./screenshots/card-form-loading.png)
+![Success](./screenshots/card-form-success.png)
+![Server Error](./screenshots/card-form-server-error.png)
+
+## Contact
+
+If you have any questions or feedback, feel free to contact me at [dev.seregin.m.v@ya.ru](mailto:dev.seregin.m.v@ya.ru)
+
+---
